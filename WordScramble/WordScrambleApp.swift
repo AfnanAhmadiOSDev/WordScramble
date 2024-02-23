a02740cbd8db18c5d3debc81d4a6d6973bbaf559
Afnan Ahmad <afnan.ahmad.iosdev@gmail.com>
2024-02-23 09:55:46 +0500

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Afnan Ahmad on 21/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
